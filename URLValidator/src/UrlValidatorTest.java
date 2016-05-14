8e075e0dae7b4e2e63079223f68a4267cddca8db
John Doe <johndoe@example.com>
2016-05-13 21:57:23 -0700

    firstTests

--- URLValidator/src/UrlValidatorTest.java
+++ URLValidator/src/UrlValidatorTest.java
@@ -41,7 +41,30 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
+	   //System.out.println(urlVal.isValid("http://www.amazon.com"));
+	   
+	   assertTrue(urlVal.isValid("http://www.amazon.com"));
+	   assertTrue(urlVal.isValid("HTTP://WWW.AMAZON.COM"));
+	   assertTrue(urlVal.isValid("http://amazon.com"));
+	   assertTrue(urlVal.isValid("HTTP://AMAZON.COM"));
+	   assertTrue(urlVal.isValid("https://amazon.com"));
+	   assertTrue(urlVal.isValid("HTTPS://AMAZON.COM"));
+	   
+	   assertTrue(urlVal.isValid("http://www.amazon.net"));
+	   assertTrue(urlVal.isValid("HTTP://WWW.AMAZON.NET"));
+	   assertTrue(urlVal.isValid("http://amazon.net"));
+	   assertTrue(urlVal.isValid("HTTP://AMAZON.NET"));
+	   assertTrue(urlVal.isValid("https://amazon.net"));
+	   assertTrue(urlVal.isValid("HTTPS://AMAZON.NET"));
+	   
+	   assertTrue(urlVal.isValid("http://www.amazon.org"));
+	   assertTrue(urlVal.isValid("HTTP://WWW.AMAZON.ORG"));
+	   assertTrue(urlVal.isValid("http://amazon.org"));
+	   assertTrue(urlVal.isValid("HTTP://AMAZON.ORG"));
+	   assertTrue(urlVal.isValid("https://amazon.org"));
+	   assertTrue(urlVal.isValid("HTTPS://AMAZON.ORG"));
+	   
+	   //assertTrue(urlVal.isValid("amazon.com"));
 	   
 	   
    }
@@ -50,17 +73,173 @@ public class UrlValidatorTest extends TestCase {
    public void testYourFirstPartition()
    {
 	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   assertTrue(urlVal.isValid("http://www.amazon.org"));
+	   assertTrue(urlVal.isValid("ftp://www.amazon.org"));
+	   assertTrue(urlVal.isValid("h3t://www.amazon.org"));
+	  
+	   
+	   assertTrue(urlVal.isValid("http://www.google.com"));
+	   assertTrue(urlVal.isValid("http://www.go.com"));
+	   assertTrue(urlVal.isValid("http://www.go.au"));
+	   assertTrue(urlVal.isValid("http://0.0.0.0"));
+	   assertTrue(urlVal.isValid("http://255.255.255.255"));
+	   assertTrue(urlVal.isValid("http://256.256.256.256"));
+	   assertTrue(urlVal.isValid("http://255.com"));
+	   assertTrue(urlVal.isValid("http://go.cc"));
+	   
+	   assertTrue(urlVal.isValid("http://www.google.com:80"));
+	   //assertTrue(urlVal.isValid("http://www.google.com:65535")); //BUG
+	   assertTrue(urlVal.isValid("http://www.google.com:0"));
+	   //assertTrue(urlVal.isValid("http://www.google.com:65636")); //BUG
+	   
+	   assertTrue(urlVal.isValid("http://www.google.com/test1"));
+	   assertTrue(urlVal.isValid("http://www.google.com/t123"));
+	   assertTrue(urlVal.isValid("http://www.google.com/$23"));
+	   assertTrue(urlVal.isValid("http://www.google.com/test1/"));
+	   assertTrue(urlVal.isValid("http://www.google.com/test1/file"));
+	   assertTrue(urlVal.isValid("http://www.google.com/t123/file"));
+	   assertTrue(urlVal.isValid("http://www.google.com/$23/test1"));
+
+	   //assertTrue(urlVal.isValid("http://www.yelp.com/?action=view")); //BUG
+	   //assertTrue(urlVal.isValid("http://www.yelp.com/?action=edit&mode=up"));
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	   
    }
    
    public void testYourSecondPartition(){
 	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   
+	   assertFalse(urlVal.isValid("////"));
+	   assertFalse(urlVal.isValid("3ht://www.google.com"));
+	   assertFalse(urlVal.isValid("htp:/www.google.com")); 
+	   assertFalse(urlVal.isValid("http:/www.google.com"));
+	   assertFalse(urlVal.isValid("://www.google.com"));
+	   //assertFalse(urlVal.isValid("http://256.256.256.256")); //BUG
+	   assertFalse(urlVal.isValid("://1.2.3.4.5"));
+	   assertFalse(urlVal.isValid("://1.2.3.4."));
+	   assertFalse(urlVal.isValid("://1.2.3"));
+	   assertFalse(urlVal.isValid("://.1.2.3.4"));
+	   assertFalse(urlVal.isValid("http://www.go.a"));
+	   assertFalse(urlVal.isValid("http://www.go.a1a"));
+	   assertFalse(urlVal.isValid("http://www.go.a1a"));
+	   //assertFalse(urlVal.isValid("http://www.go.cc")); //BUG
+	   assertFalse(urlVal.isValid("http://go.1aa"));
+	   assertFalse(urlVal.isValid("http://www.aaa"));
+	   assertFalse(urlVal.isValid("http://www.aaa."));
+	   assertFalse(urlVal.isValid("http://aaa"));
+	   assertFalse(urlVal.isValid("http://www.google.com:-1"));
+	   assertFalse(urlVal.isValid("http://www.google.com:65a"));
+	   assertFalse(urlVal.isValid("http://www.google.com/.."));
+	   assertFalse(urlVal.isValid("http://www.google.com/../"));
+	   assertFalse(urlVal.isValid("http://www.google.com/..//file"));
+	   assertFalse(urlVal.isValid("http://www.google.com/test1//file"));
+	   assertFalse(urlVal.isValid("http://www.google.com/.."));
+	   assertFalse(urlVal.isValid("http://www.google.com/../"));
+	   //assertFalse(urlVal.isValid("http://www.google.com/#")); //BUG
+	   assertFalse(urlVal.isValid("http://www.google.com/../file"));
+	   assertFalse(urlVal.isValid("http://www.google.com/..//file"));
+	   //assertFalse(urlVal.isValid("http://www.google.com/#/file")); //BUG
+
+	   
+	   
+
    }
    
+
    
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
+	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+	   //for(int i = 0;i<10000;i++)
+		   for(int i = 0;i<1;i++)
 	   {
+		   
+		   String testUrl = "";
+		   
+		   System.out.println(firstPart[0].item);
+		   
+
+		   for (int firstIndex = 0; firstIndex < firstPart.length ; firstIndex++)
+			   
+		   {
+			   for (int secondIndex = 0; secondIndex < secondPart.length ; secondIndex++)
+				   
+			   {
+				   for (int thirdIndex = 0; thirdIndex < thirdPart.length ; thirdIndex++)
+					   
+				   {
+					   for (int fourthIndex = 0; fourthIndex < fourthPart.length ; fourthIndex++)
+						   
+					   {
+						   for (int fifthIndex = 0; fifthIndex < fifthPart.length ; fifthIndex++)
+							   
+						   {
+							   
+							   testUrl = testUrl + firstPart[firstIndex].item;
+							   testUrl = testUrl + secondPart[secondIndex].item;
+							   testUrl = testUrl + thirdPart[thirdIndex].item;
+							   testUrl = testUrl + fourthPart[fourthIndex].item;
+							   testUrl = testUrl + fifthPart[fifthIndex].item;
+							   
+							   System.out.println("testing: " + testUrl + "\n");
+							   
+							   if (firstPart[firstIndex].valid == true && secondPart[secondIndex].valid == true && thirdPart[thirdIndex].valid == true && fourthPart[fourthIndex].valid == true && fifthPart[fifthIndex].valid == true)
+							   {
+								   
+								   System.out.println("Expected: true, got: " + urlVal.isValid(testUrl));
+								   
+								   //assertTrue(urlVal.isValid(testUrl));
+								   
+							   }
+							   
+							   else if (firstPart[firstIndex].valid == false || secondPart[secondIndex].valid == false || thirdPart[thirdIndex].valid == false || fourthPart[fourthIndex].valid == false || fifthPart[fifthIndex].valid == false)
+							   {
+								   
+								   System.out.println("Expected: false, got: " + urlVal.isValid(testUrl));
+								   
+								   //assertFalse(urlVal.isValid(testUrl));
+								   
+							   }
+							   testUrl = "";
+							   
+						   }
+					   }
+				   }
+			   }
+			   
+
+			   
+			   testUrl = testUrl + firstPart[i].item;
+			   testUrl = testUrl + secondPart[i].item;
+			   testUrl = testUrl + thirdPart[i].item;
+			   testUrl = testUrl + fourthPart[i].item;
+			   testUrl = testUrl + fifthPart[i].item;
+			   
+			   System.out.println("testing: " + testUrl + "\n");
+			   
+			   testUrl = "";
+			   
+			   
+		   }
+		   
+		   
 		   
 	   }
    }
@@ -76,5 +255,59 @@ public class UrlValidatorTest extends TestCase {
     * @param testObjects Used to create a url.
     */
    
+   ResultPair[] firstPart = {
+		   new ResultPair("http://", true),
+           new ResultPair("ftp://", true),
+           new ResultPair("h3t://", true),
+           new ResultPair("3ht://", false),
+           new ResultPair("http:/", false),
+           new ResultPair("http:", false),
+           new ResultPair("http/", false),
+           new ResultPair("://", false),
+           new ResultPair("", true)
+    };
+   
+   
+	
 
+	ResultPair[] secondPart = {
+		new ResultPair("google.com", true),
+        new ResultPair("amazon.com", true),
+        new ResultPair("ebay.com", true),
+        new ResultPair("", true)
+        
+	};
+	
+	ResultPair[] thirdPart = {
+		new ResultPair(":90", true),
+	    new ResultPair(":95", true),
+	    new ResultPair(":100", true),
+	    new ResultPair("", true)
+	};
+	
+	ResultPair[] fourthPart = {
+			
+        new ResultPair("/$23", true),
+        new ResultPair("/..", false),
+        new ResultPair("/../", false),
+        new ResultPair("/test1/", true),
+	};
+	
+	ResultPair[] fifthPart = {
+			
+			
+			
+		new ResultPair("?action=view", true),
+        new ResultPair("?action=edit&mode=up", true),
+        new ResultPair("", true)
+};
+	
+	
+	
+	
+	
+	
+	
 }
+
+	
